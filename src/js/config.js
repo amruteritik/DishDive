26651b0683081184d4195862d4af7da55649f59e
ritik <amruteritikofficial@gmail.com>
2024-01-20 04:24:41 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5a0e03d0-3ce1-44b2-a033-179ca0ae5fe4';
+export const MODAL_CLOSE_SEC = 2.5;
